5a07452850c48648ff8073d4e72bf2f70737d68f
BobLouis <bob900316@gmail.com>
2020-11-27 11:33:45 +0800

    commit quick_sort original

---
+++ quick_sort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N (sizeof(a)/sizeof(a[0]))
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int split(int a[],int low,int high,int pivot){
+    pivot=a[pivot];
+    while(low<high){
+        while(a[low]<pivot&&low<high){
+            low++;
+        }
+        while(a[high]>pivot&&low<high){
+            high--;
+        }
+        if(a[high]<a[low]){
+            swap(&a[high],&a[low]);
+        }
+    }
+//    for(int i=0;i<9;i++){
+//        printf("%d ",a[i]);
+//    }
+//    printf("   index=%d ",high);
+//    printf("\n");
+    return low;
+}
+void sort(int a[] ,int low,int high){
+    int pivot,index;
+    if(low>=high){
+        return;
+    }
+    pivot=(low+high)/2;
+    index=split(a,low,high,pivot);
+    sort(a,low,index-1);
+//    printf("sort high");
+    sort(a,index+1,high);
+}
+
+
+int main() {
+    int a[] = {3, 1, 5, 4, 2, 9, 8, 7, 6,44,33,35,32,11,23,15};
+    int index, pivot;
+    sort(a, 0, N - 1);
+//    pivot=(N-1)/2;
+//    index=split(a,0,N-1,pivot);
+    // list<int n>
+    for(int i=0;i<N;i++){
+        printf("%d ",a[i]);
+    }
+
+    return 0;
+}
